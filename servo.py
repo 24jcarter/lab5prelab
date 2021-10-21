d51b3d4afc660b056d62cfcd7a5071fc658eb5d7
24jcarter <91288010+24jcarter@users.noreply.github.com>
2021-10-21 00:46:26 +0000

    changed duty

--- servo.py
+++ servo.py
@@ -7,8 +7,8 @@ GPIO.setup(pwmPin, GPIO.OUT)
 
 # set min & max % duty cycles (5 and 10 are default values, but play
 # around to find optimum values for your motor)
-dcMin = 3
-dcMax = 12
+dcMin = 5
+dcMax = 10
 
 pwm = GPIO.PWM(pwmPin, 50) # PWM object at 50 Hz (20 ms period)
 pwm.start(0)
@@ -20,5 +20,5 @@ try:
       print(dc)
       time.sleep(0.5)
 except KeyboardInterrupt:
-  print("closing")
+  print("\nclosing")
 GPIO.cleanup()
